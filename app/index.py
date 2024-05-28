b88da320d709f5b137996939a53dc8fb3dfa210f
RajveerSodhi <rajveersodhi03@gmail.com>
2024-05-27 22:54:57 -0700

    Update index.py

--- app/index.py
+++ app/index.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+def get_mongo_conection():
+    connection = MongoDBConnection()
+    try:
+        yield connection
+    finally:
+        connection.close()
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
